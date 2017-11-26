991c5940250bcfd5278ec365a5186dbf1f608fdc
github username <github email address>
2017-11-26 13:38:46 -0500

    events page design updates

--- src/components/events/EventsLister.js
+++ src/components/events/EventsLister.js
@@ -1,39 +1,50 @@
 import React from 'react'
-import { Button, Card, Image } from 'semantic-ui-react'
+import { Button, Card, Image, Dimmer, Loader, Segment, Grid } from 'semantic-ui-react'
 
 
 const EventsLister = (props) => {
-  if(props.userEvents.length === 0) {
-    return null
+  if(props.userEvents.length === 0 || props.userEvents === undefined ) {
+    return (
+      <div className= 'loading-div'>
+        <Segment>
+          <Dimmer active>
+            <Loader size='huge' indeterminate>Preparing Files</Loader>
+          </Dimmer>
+        </Segment>
+      </div>
+    )
   } else {
-    return props.userEvents.map((event) => {
-      return(
-        <div className='event-list'>
-          <Card.Group>
-            <Card>
-              <Card.Content>
-                <Image floated='right' size='small' src={event.images[0].url} />
-                <Card.Header>
-                  {event.name}
-                </Card.Header>
-                <Card.Meta>
-                  Friends of Elliot
-                </Card.Meta>
-                <Card.Description>
-                  Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <div className='ui two buttons'>
-                  <Button basic color='green'>Approve</Button>
-                  <Button basic color='red'>Decline</Button>
-                </div>
-              </Card.Content>
-            </Card>
-          </Card.Group>
-        </div>
-      )
-    })
+    return <Grid stackable columns={2}>
+      {props.userEvents.map((event) => {
+        return(
+          <Grid.Column>
+            <Card.Group>
+              <Card>
+                <Card.Content>
+                  <Image floated='right' size='small' src={event.images[0].url} />
+                  <Card.Header>
+                    {event.name}
+                  </Card.Header>
+                  <Card.Meta>
+                  </Card.Meta>
+                  <Card.Description>
+                    <p>{event._embedded.venues[0].name}</p>
+                    <p>{event.dates.start.localDate}</p>
+                    <p>{event.dates.start.localTime}</p>
+                    <a href={event.url} >Buy Tickets</a>
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <div className='ui two buttons'>
+                    <Button basic className='large' color='green'>Add to To-Do List</Button>
+                  </div>
+                </Card.Content>
+              </Card>
+            </Card.Group>
+          </Grid.Column>
+        )
+      })}
+    </Grid>
   }
 }
 
