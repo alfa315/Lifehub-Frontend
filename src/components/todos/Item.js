b6f5881e7224c92d942bf0c253789afbb5521b9d
github username <github email address>
2017-12-04 16:50:56 -0500

    added color to table based on deadline date and current date

--- src/components/todos/Item.js
+++ src/components/todos/Item.js
@@ -4,8 +4,20 @@ import EditItemButton from './EditItemButton.js'
 import CompleteItemButton from './CompleteItemButton.js'
 
 const Item = (props) => {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth()+1;
+  let yyyy = today.getFullYear();
+  if(dd<10) {
+    dd = '0'+dd
+  }
+  if(mm<10) {
+      mm = '0'+mm
+  }
+  today = yyyy + '-' + mm + '-' + dd;
+  let dateDifference = (new Date(props.item.deadline) - new Date(today)) / 86400000
   return(
-		<tr style={{textAlign:"center"}}>
+		<tr style={{textAlign:"center"}} className={dateDifference <= 0 ? 'error' : dateDifference > 0 && dateDifference <= 3 ? 'warning' : 'normal'}>
 			<td>{props.item.todo_name}</td>
 			<td>{props.item.todo_type}</td>
 			<td>{props.item.todo_description}</td>
